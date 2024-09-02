cfaf8e1b2650e346780d2dfb098fd6f32ff0ebbd
syr <sufr3b9@foxmail.com>
2024-09-03 01:50:47 +0800

    0903

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,26 @@
 
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import  getImageDimensions from 'next/image';
 import '../styles/global.css';
 
-import { ArrowBottomLeftIcon, ArrowBottomRightIcon, ArrowDownIcon, ArrowLeftIcon, ArrowRightIcon, RadiobuttonIcon,
-  ArrowTopLeftIcon, ArrowUpIcon, ArrowTopRightIcon
+import {
+  ArrowBottomLeftIcon, ArrowBottomRightIcon, ArrowDownIcon, ArrowLeftIcon, ArrowRightIcon, RadiobuttonIcon,
+  ArrowTopLeftIcon, ArrowUpIcon, ArrowTopRightIcon, Cross2Icon
 } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 
 const folders = ['p111', 'p112', 'p113', 'p121', 'p122', 'p123', 'p131', 'p132', 'p133'];
-// const routeFiles = ['route111', 'route112', 'route113', 'route121', 'route122', 'route123', 'route131', 'route132', 'route133'];
+
 export default function Home() {
   const [images, setImages] = useState<string[]>([]);
-  const [loading, setLoading] = useState(true);  // 增加加载状态
+  const [loading, setLoading] = useState(true);
   const [selectedFolder, setSelectedFolder] = useState('p122');
+  const [selectedImage, setSelectedImage] = useState<string | null>(null);
+  
 
   useEffect(() => {
-    setLoading(true);  // 开始加载新图片时设置为true
-    //fetch(`/api/images?folder=${selectedFolder}`)
+    setLoading(true);
     fetch(`/api/${selectedFolder}?folder=${selectedFolder}`)
       .then(res => res.json())
       .then(data => {
@@ -28,13 +31,39 @@ export default function Home() {
         } else {
           setImages(data);
         }
-        setLoading(false);  // 图片加载完成后设置为false
+        setLoading(false);
       });
   }, [selectedFolder]);
 
+  /* async function getImageSize(src) {
+    const { width, height } = await getImageDimensions(src);
+    console.log(`图像宽度：${width}px`);
+    console.log(`图像高度：${height}px`);
+    console.log('say sth');
+  } */
+
+  const handleImageClick = (src: string) => {
+    const img = new window.Image();
+    img.src = src;
+
+    img.onload = () => {
+      
+      setSelectedImage(src);
+    };
+    // getImageSize(src);
+  };
+
+  const handleClose = () => {
+    setSelectedImage(null);
+  };
+
+  // const isHeightGreater = imageDimensions.height > imageDimensions.width;
+
   return (
     <main className="flex h-screen">
-      <div className="w-1/4 p-10 border-r justify-center">
+
+      {/* 左侧 */}
+      <div className="w-1/4 p-10 border-r justify-center" style={{minWidth:'180px'}}>
         <h2 className="mt-4 scroll-m-20 pb-0 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
           TFL v1.0.0
         </h2>
@@ -43,30 +72,25 @@ export default function Home() {
         </p>
         <p className="text-sm text-muted-foreground py-4">
           本网站基于九个头部朝向范围，对互联网上部分现存的托马斯·穆勒照片做了分类。
-          
         </p>
         <p className="text-sm text-muted-foreground py-4">
-          
           点击下方任一按钮，即可选择按钮所指示的特定头部朝向范围的照片。
         </p>
         <div className="flex flex-col space-y-2 justify-between items-center pb-4">
           <div className="flex space-x-2">
             <Button size="icon"
               onClick={() => setSelectedFolder('p111')}
-              variant={`${selectedFolder === 'p111' ?
-                "secondary" : "outline"}`}  >
+              variant={`${selectedFolder === 'p111' ? "secondary" : "outline"}`}  >
               <ArrowTopLeftIcon className="h-4 w-4" />
             </Button>
             <Button size="icon"
               onClick={() => setSelectedFolder('p112')}
-              variant={`${selectedFolder === 'p112' ?
-                "secondary" : "outline"}`}  >
+              variant={`${selectedFolder === 'p112' ? "secondary" : "outline"}`}  >
               <ArrowUpIcon className="h-4 w-4" />
             </Button>
             <Button size="icon"
               onClick={() => setSelectedFolder('p113')}
-              variant={`${selectedFolder === 'p113' ?
-                "secondary" : "outline"}`}  >
+              variant={`${selectedFolder === 'p113' ? "secondary" : "outline"}`}  >
               <ArrowTopRightIcon className="h-4 w-4" />
             </Button>
           </div>
@@ -74,20 +98,17 @@ export default function Home() {
           <div className="flex space-x-2">
             <Button size="icon"
               onClick={() => setSelectedFolder('p121')}
-              variant={`${selectedFolder === 'p121' ?
-                "secondary" : "outline"}`}  >
+              variant={`${selectedFolder === 'p121' ? "secondary" : "outline"}`}  >
               <ArrowLeftIcon className="h-4 w-4" />
             </Button>
             <Button size="icon"
               onClick={() => setSelectedFolder('p122')}
-              variant={`${selectedFolder === 'p122' ?
-                "secondary" : "outline"}`}  >
+              variant={`${selectedFolder === 'p122' ? "secondary" : "outline"}`}  >
               <RadiobuttonIcon className="h-4 w-4" />
             </Button>
             <Button size="icon"
               onClick={() => setSelectedFolder('p123')}
-              variant={`${selectedFolder === 'p123' ?
-                "secondary" : "outline"}`}  >
+              variant={`${selectedFolder === 'p123' ? "secondary" : "outline"}`}  >
               <ArrowRightIcon className="h-4 w-4" />
             </Button>
           </div>
@@ -95,29 +116,42 @@ export default function Home() {
           <div className="flex space-x-2">
             <Button size="icon"
               onClick={() => setSelectedFolder('p131')}
-              variant={`${selectedFolder === 'p131' ?
-                "secondary" : "outline"}`}  >
+              variant={`${selectedFolder === 'p131' ? "secondary" : "outline"}`}  >
               <ArrowBottomLeftIcon className="h-4 w-4" />
             </Button>
             <Button size="icon"
               onClick={() => setSelectedFolder('p132')}
-              variant={`${selectedFolder === 'p132' ?
-                "secondary" : "outline"}`}  >
+              variant={`${selectedFolder === 'p132' ? "secondary" : "outline"}`}  >
               <ArrowDownIcon className="h-4 w-4" />
             </Button>
             <Button size="icon"
               onClick={() => setSelectedFolder('p133')}
-              variant={`${selectedFolder === 'p133' ?
-                "secondary" : "outline"}`}  >
+              variant={`${selectedFolder === 'p133' ? "secondary" : "outline"}`}  >
               <ArrowBottomRightIcon className="h-4 w-4" />
             </Button>
           </div>
         </div>
       </div>
-      <div className="w-3/4 h-full p-4 bg-zinc-50" style={{ overflowY: 'scroll' }}>
-        <div className="grid grid-cols-3 gap-4">
+
+      {/* 右侧 */}
+      
+      <div className="w-3/4 h-full" style={{ overflowY: 'scroll' }}>
+      <header className="flex justify-between items-center border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60"
+            style={{
+              position: 'fixed', borderBottom: '0.4px solid #dddddd',
+              width: '90%', padding: 10, paddingLeft: 40, paddingRight: 40, zIndex: 40
+            }}>
+            <div className="flex items-center text-slate-700 font-semibold">
+                <p>Gallery</p>
+            </div>
+            <div className="flex gap-8">
+
+              
+            </div>
+          </header>
+        <div className="p-4 pt-14 bg-zinc-50">
+        <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 gap-4">
           {loading ? (
-            // 显示占位图
             Array(9).fill(0).map((_, index) => (
               <div key={index} className="relative w-full h-60 bg-zinc-100 animate-pulse rounded-lg"></div>
             ))
@@ -125,7 +159,8 @@ export default function Home() {
             <p>No images available</p>
           ) : (
             images.map((src, index) => (
-              <div key={index} className="relative w-full h-60 opacity-0 transition-opacity duration-500" style={{ animation: 'fadeIn 2s forwards' }}>
+              <div key={index} className="relative w-full h-60 opacity-0 transition-opacity duration-500" style={{ animation: 'fadeIn 2s forwards', cursor: 'pointer' }}
+                onClick={() => handleImageClick(src)}>
                 <Image
                   src={src}
                   alt={`Image ${index + 1}`}
@@ -136,6 +171,34 @@ export default function Home() {
             ))
           )}
         </div>
+        </div>
+        {selectedImage && (
+          <div
+            className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center z-50"
+            onClick={handleClose}
+          >
+            <Button
+                
+                onClick={handleClose}
+                size="icon"
+                variant="outline"
+                className="absolute top-4 right-4 rounded-full"
+              >
+            <Cross2Icon className="w-4 h-4" />
+            </Button>
+            <div className="relative px-20 py-20">
+              <Image
+                src={selectedImage}
+                alt="Selected Image"
+               
+                height={500}
+                width={500}
+                className="m-4"
+              />
+              
+            </div>
+          </div>
+        )}
       </div>
     </main>
   );
